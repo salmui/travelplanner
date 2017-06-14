c0d83fa2355a7b1cbe97edf9cb715e9a0af9c70d
ArbutusNate <arbutusnate@gmail.com>
2017-06-13 21:12:20 -0700

    Working Ajax call with example syntax

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,6 @@
 // Variables
 var database = firebase.database()
-
+var venueid = ''
 
 $.ajax({
   url: 'https://www.eventbriteapi.com/v3/events/search/',
@@ -8,14 +8,16 @@ $.ajax({
   data: {
   	token: '75JDM6P6R2M2PFYEECJ3',
   	categories: '103',
-  	sort_by: '-distance'
+  	sort_by: '-distance',
+  	'location.address': 'San Francisco'
   }
 }).done(function(response){
-	console.log('--------Response 1---------')
+	console.log('--------Test 1---------')
 	console.log(response)
 	for (var i = 0; i < response.events.length; i++){
-		var namePrint = response.events[i].name.text		
+		var namePrint = response.events[i].name.text
+		// venueid = response.events[i].venue_id		
 		console.log(namePrint)
-	};
+	  };		
 	console.log('---------------------------')
 })
