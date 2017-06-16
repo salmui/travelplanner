76110e841c43b3598b644a1ba78c6a1836a8081a
ArbutusNate <arbutusnate@gmail.com>
2017-06-15 18:13:12 -0700

    Adding and testing ajax calls for eventbrite API

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,23 +1,53 @@
 // Variables
-var database = firebase.database()
-var venueid = ''
+  var database = firebase.database()
+  var venueid = ''
 
-// $.ajax({
-//   url: 'https://www.eventbriteapi.com/v3/events/search/',
-//   method: 'GET',
-//   data: {
-//   	token: '75JDM6P6R2M2PFYEECJ3',
-//   	categories: '103',
-//   	sort_by: '-distance',
-//   	'location.address': 'San Francisco'
-//   }
-// }).done(function(response){
-// 	console.log('--------Test 1---------')
-// 	console.log(response)
-// 	for (var i = 0; i < response.events.length; i++){
-// 		var namePrint = response.events[i].name.text
-// 		// venueid = response.events[i].venue_id		
-// 		console.log(namePrint)
-// 	  };		
-// 	console.log('---------------------------')
-// })
+
+
+
+
+
+
+// base eventbrite API
+  $.ajax({
+    url: 'https://www.eventbriteapi.com/v3/events/search/',
+    method: 'GET',
+    data: {
+      token: '75JDM6P6R2M2PFYEECJ3',
+      categories: '103',
+      sort_by: '-distance',
+      'location.address': 'San Francisco'
+    }
+  }).done(function(response){
+      console.log(response)
+      for (var i = 0; i < 10; i++){
+          var namePrint = response.events[i].name.text
+          var idPrint = response.events[i].venue_id
+          console.log(namePrint + '---' + idPrint + '---' + i)
+            $.ajax({
+              url: 'https://www.eventbriteapi.com/v3/venues/' + idPrint + '/',
+              method: 'GET',
+              data: {
+                token: '75JDM6P6R2M2PFYEECJ3'
+              }
+            }).done(function(response){
+              var venueName = response.name
+              console.log('--------Event ' + i + ' -----------')
+              // console.log('Event Name = ' + namePrint)
+              // console.log('venue_id = ' + idPrint)
+              // console.log('Venue Name = ' + venueName)
+            })
+
+        };
+  })
+
+//eventbrite venue API
+  $.ajax({
+    url: 'https://www.eventbriteapi.com/v3/venues/19770605/',
+    method: 'GET',
+    data: {
+      token: '75JDM6P6R2M2PFYEECJ3'
+    }
+  }).done(function(response){
+    console.log(response)
+  })
